47d5b21c06d365f154ecea23ed6241cca3dfe74a
Vincent JARDIN <vjardin@free.fr>
2020-11-07 18:24:47 +0000

    localize namespace: Vjardin\something
    The library should be insto Vjardin namspace.

--- src/SayHello.php
+++ src/SayHello.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace HelloWorld;
+namespace Vjardin\HelloWorld;
 
 class SayHello {
     public static function world(): string {
--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use HelloWorld\SayHello;
+use Vjardin\HelloWorld\SayHello;
 
 echo SayHello::world();
